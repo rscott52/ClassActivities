83e88adfcc17d9ece3c396b779080064bb30c8e8
Ryan Scott <ryanamscott52@gmail.com>
2023-11-15 18:41:53 -0600

    Adds Math Operators And Data Coercion Intro

---
+++ week2/2.3.1-MathOperatorsAndDataCoercionIntro.js
@@ -0,0 +1,17 @@
+// What should we expect to print?
+
+// Basic Math
+console.log(1 + 2 * 3); // should print 7
+console.log((1 + 2) * 3); //should print 9
+console.log(40 / 5 - 2 ** 3); // should print 0
+console.log(6 % 2); //should print 0
+
+// Adding/Concatenating Strings
+console.log("10" + "5"); //should print "105"
+console.log("Hello " + "world"); //should print "hello world"
+
+// Data Coercion
+console.log("2" + 2); //should print "22"
+console.log(2 + true); //should print 3
+console.log(true + true + "true" + "false"); //should print "2truefalse"
+console.log(1 + 1 + "2" + "3"); // should print "223"
