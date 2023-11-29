e5f79165f4d6ff251d625bce6fead53aeac062da
Ryan Scott <ryanamscott52@gmail.com>
2023-11-28 20:19:01 -0600

    Updates week4 folders and completes 4.2.1 and 4.2.2 class activities

---
+++ week4/4.2.1-SelectingDOMElements.js
@@ -0,0 +1,15 @@
+// select all unordered list (ul) elements
+const byTagUl = document.getElementsByTagName("ul");
+console.log(byTagUl);
+
+const queryUl = document.querySelectorAll("ul");
+console.log(queryUl);
+// select all elements with the class "class-week"
+document.getElementsByClassName("class-week");
+document.querySelectorAll(".class-week");
+// select all elements with the class "nav-bar"
+document.getElementsByClassName("nav-bar");
+document.querySelectorAll(".nav-bar");
+// select the element with the id "dog-picture" and save it to a variable
+document.getElementById("dog-picture");
+document.querySelector("#dog-picture");
---
+++ week4/4.2.2-NodeRelationships.js
@@ -0,0 +1,15 @@
+// select the root node
+document.getElementsByTagName("body");
+document.querySelector("body");
+// select the last child of the root node
+document.getElementsByTagName("body").lastElementChild;
+document.querySelector("body").lastElementChild;
+// select all the children of the body element
+document.getElementsByTagName("body").children;
+document.querySelector("body").children;
+// select the next sibling of the h2 node
+document.getElementsByTagName("h2").nextElementSibling;
+document.querySelector("h2").nextElementSibling;
+// select the parent element of the h1 node
+document.getElementsByTagName("h1").parentElement;
+document.querySelector("h1").parentElement;
